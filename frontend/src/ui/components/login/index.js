a709f9526c189b0a45123a1e842f73439759b1cb
Nithirun Numnonda <nithirun1995@gmail.com>
2017-11-20 15:37:00 +0700

    Update login page

--- frontend/src/ui/components/login/index.js
+++ frontend/src/ui/components/login/index.js
@@ -5,23 +5,29 @@ import './style.css';
 
 export default class login extends Component {
   render() {
-    
+
     return (
       <div className="Login"  >
-        <Form>
         <h1 className="display-3">TutorS</h1>
-          <FormGroup>
-            <Label className="my-2" for="exampleusername">Username</Label>{' '}
-            <Input type="username" name="" id="exampleUsername" placeholder="enter your username" />
-          </FormGroup>
-          {' '}
-          <FormGroup>
-            <Label className="my-2" for="examplePassword">Password</Label>{' '}
-            <Input type="password" name="password" id="examplePassword" placeholder="enter your username" />
-          </FormGroup>
-          {' '}
-          <Button color="success">Login</Button>
-        </Form> 
+        <div className="LoginForm">
+          <Form>
+
+            <FormGroup>
+              <Label className="my-2" for="exampleusername">Username</Label>{' '}
+              <Input type="username" name="" id="exampleUsername" placeholder="enter your username" />
+            </FormGroup>
+            {' '}
+            <FormGroup>
+              <Label className="my-2" for="examplePassword">Password</Label>{' '}
+              <Input type="password" name="password" id="examplePassword" placeholder="enter your username" />
+            </FormGroup>
+            {' '}
+            <Button className="login-button" block color="success">Login</Button>
+          </Form>
+        </div>
+        <div className="RegisterForm">
+        Don't Have Account? <a href="" className="link">Create an Account.</a>
+        </div>
       </div>
     )
   }
