da480794f236e362ed056b9ea56f96b9a58a9d00
Nithirun Numnonda <nithirun1995@gmail.com>
2017-11-21 01:10:41 +0700

    update home page

--- frontend/src/ui/components/home/index.js
+++ frontend/src/ui/components/home/index.js
@@ -1,135 +1,117 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 
-import { Container, Row, Col, Jumbotron, Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { Container, Row, Col, Form, Input, Jumbotron, Button, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import './style.css';
 import TopComponent from '../TopComponent';
 
+import whiteboard from '../../materials/whiteboard.jpg';
+
 import { Link } from 'react-router-dom'
+
+
+var middleStyle = {
+  backgroundImage: `url(${whiteboard})`
+};
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <TopComponent />
+        <TopComponent
+          value={this.props.value}
+          handleSubmit={this.props.handleSubmit}
+          handleChange={this.props.handleChange}
+        />
         <MiddleComponent />
-        <Banner />
+        <Banner/>
       </div>
     )
   }
 }
 
 const MiddleComponent = () => {
-  return(
-    <Jumbotron>
-      <h1 className="display-3">ยินดีต้อนรับ!</h1>
-      <p className="lead">Easy access, Reliable, Simplicity.</p>
+  return (
+    <Jumbotron style={ middleStyle }>
+      <h1 className="Middle-header">ยินดีต้อนรับ!</h1>
+      <p className="middle-1">Easy access, Reliable, Simplicity.</p>
       <hr className="my-2" />
-      <p>จุดเริ่มต้นของการเป็นติวเตอร์เริ่มได้ที่นี่</p>
-      <p className="lead">
-        <Link to='/search' color="primary">ค้นหาติวเตอร์</Link> &nbsp;&nbsp;&nbsp;<Link to='/' color="warning">สมัครสมาชิก</Link>
+      <p className='middle-2'>จุดเริ่มต้นของการเป็นติวเตอร์เริ่มได้ที่นี่</p>
+      <p style={{ backgroundColor: 'white',width: '20%',marginLeft:'40%'}}>
+        <Link to='/search' color="primary">ค้นหาติวเตอร์</Link> &nbsp;|&nbsp;|&nbsp;<Link to='/' color="warning">สมัครสมาชิก</Link>
       </p>
     </Jumbotron>
   )
 }
 
 const Banner = () => {
-  return(
+  return (
+    <div style={ bannerStyle }>
     <Container>
       <Row>
-        <Col xs='2'>
-        <div>
-          <Card>
-            <CardBody>
-              <br />
-              <CardTitle>Banner</CardTitle>
-              <br />
-              <br />
-              <CardSubtitle>Hello guys!</CardSubtitle>
-              <CardText>Join me on this site, it's cool site you know.</CardText>
-              <br />
-            </CardBody>
-          </Card>
-        </div>
+        <Col xs='3'>
+          <div>
+            <Card style={{ backgroundColor: '#e4e4e4',borderColor: '#6666ff',padding:'5px'}}>
+              <CardBody >
+                <br />
+                <CardTitle>ภาษาอังกฤษ อ.โย</CardTitle>
+                <br />
+                <br />
+                <CardSubtitle>สอนฟัง พูด อ่าน เขียน TOEFL IELTS</CardSubtitle>
+                <CardText>พูดได้ ใช้เป็น แกรมม่าเป๊ะ ไม่พลาดทุกสนามสอบ</CardText>
+                <a href='' className="continue-read">อ่านเพิ่ม...</a>
+              </CardBody>
+            </Card>
+          </div>
         </Col>
-        <Col xs='2'>
-        <div>
-          <Card>
-            <CardBody>
-              <br />
-              <CardTitle>Banner</CardTitle>
-              <br />
-              <br />
-              <CardSubtitle>Hello guys!</CardSubtitle>
-              <CardText>Join me on this site, it's cool site you know.</CardText>
-              <br />
-            </CardBody>
-          </Card>
-        </div>
-        </Col>
-        <Col xs='2'>
-        <div>
-          <Card>
-            <CardBody>
-              <br />
-              <CardTitle>Banner</CardTitle>
-              <br />
-              <br />
-              <CardSubtitle>Hello guys!</CardSubtitle>
-              <CardText>Join me on this site, it's cool site you know.</CardText>
-              <br />
-            </CardBody>
-          </Card>
-        </div>
-        </Col>
-        <Col xs='2'>
-        <div>
-          <Card>
-            <CardBody>
-              <br />
-              <CardTitle>Banner</CardTitle>
-              <br />
-              <br />
-              <CardSubtitle>Hello guys!</CardSubtitle>
-              <CardText>Join me on this site, it's cool site you know.</CardText>
-              <br />
-            </CardBody>
-          </Card>
-        </div>
-        </Col>
-        <Col xs='2'>
-        <div>
-          <Card>
-            <CardBody>
-              <br />
-              <CardTitle>Banner</CardTitle>
-              <br />
-              <br />
-              <CardSubtitle>Hello guys!</CardSubtitle>
-              <CardText>Join me on this site, it's cool site you know.</CardText>
-              <br />
-            </CardBody>
-          </Card>
-        </div>
-        </Col>
-        <Col xs='2'>
-        <div>
-          <Card>
-            <CardBody>
-              <br />
-              <CardTitle>Banner</CardTitle>
-              <br />
-              <br />
-              <CardSubtitle>Hello guys!</CardSubtitle>
-              <CardText>Join me on this site, it's cool site you know.</CardText>
-              <br />
-            </CardBody>
-          </Card>
-        </div>
+        <Col xs='3'>
+          <div>
+            <Card style={{ backgroundColor: '#e4e4e4',borderColor: '#6666ff',padding:'5px'}}>
+              <CardBody>
+                <br />
+                <CardTitle>คณิตศาสตร์ กับ พี่ลุค</CardTitle>
+                <br />
+                <br />
+                <CardSubtitle>ติวเข้ม เตรียมสอบ Entrance !</CardSubtitle>
+                <CardText>ตะลุยโจทย์จริง ด้วยประสบการณ์มากกว่า 20 ปี</CardText>
+                <a href='' className="continue-read">อ่านเพิ่ม...</a>
+              </CardBody>
+            </Card>
+          </div>
         </Col>
 
-        
+        <Col xs='3'>
+          <div>
+            <Card style={{ backgroundColor: '#e4e4e4',borderColor: '#6666ff',padding:'5px'}}>
+              <CardBody>
+                <br />
+                <CardTitle>สนุกสังคม อ.เบน</CardTitle>
+                <br />
+                <br />
+                <CardSubtitle>มัธยมต้น-ปลาย เตรียมสอบ O-NET</CardSubtitle>
+                <CardText>เนื้อหาแน่น เรียนสนุก ไม่พอใจยินดีคืนเงิน</CardText>
+                <a href='' className="continue-read">อ่านเพิ่ม...</a>
+              </CardBody>
+            </Card>
+          </div>
+        </Col>
+        <Col xs='3'>
+          <div>
+            <Card style={{ backgroundColor: '#e4e4e4',borderColor: '#6666ff',padding:'5px'}}>
+              <CardBody>
+                <br />
+                <CardTitle>ฟิสิกส์ พี่พาทีน</CardTitle>
+                <br />
+                <br />
+                <CardSubtitle>ติวเข้ม เตรียมสอบเข้ามหาฯลัย</CardSubtitle>
+                <CardText>ไฟฟ้ากระแส ไฟฟ้าสถิต ของไหล กลศาสตร์</CardText>
+                <a href='' className="continue-read">อ่านเพิ่ม...</a>
+              </CardBody>
+            </Card>
+          </div>
+        </Col>
       </Row>
     </Container>
+    </div>
   )
 }
 
