21c127ae6f92df9a7aa5a70540d7539d93536c65
Nithirun Numnonda <nithirun1995@gmail.com>
2017-11-21 01:08:16 +0700

    edit login page

--- frontend/src/ui/components/login/index.js
+++ frontend/src/ui/components/login/index.js
@@ -7,29 +7,26 @@ export default class login extends Component {
   render() {
 
     return (
-      <div>
-        <TopComponent />
-        <div className="Login"  >
-          <h1 className="display-3">TutorS</h1>
-          <div className="LoginForm">
-            <Form>
+      <div className="Login">
+        <h1 className="display-3"><label className='little'>เข้าสู่ระบบจับคู่ติวเตอร์</label><br/>TutorS</h1>
+        <div className="LoginForm">
+          <Form>
 
-              <FormGroup>
-                <Label className="my-2" for="exampleusername">Username</Label>{' '}
-                <Input type="username" name="" id="exampleUsername" placeholder="enter your username" />
-              </FormGroup>
-              {' '}
-              <FormGroup>
-                <Label className="my-2" for="examplePassword">Password</Label>{' '}
-                <Input type="password" name="password" id="examplePassword" placeholder="enter your username" />
-              </FormGroup>
-              {' '}
-              <Button className="login-button" block color="success">Login</Button>
-            </Form>
-          </div>
-          <div className="RegisterForm">
-          Don't Have Account? <a href="" className="link">Create an Account.</a>
-          </div>
+            <FormGroup>
+              <Label className="my-2" for="exampleusername">Username</Label>{' '}
+              <Input type="username" name="" id="exampleUsername" placeholder="enter your username" />
+            </FormGroup>
+            {' '}
+            <FormGroup>
+              <Label className="my-2" for="examplePassword">Password</Label>{' '}
+              <Input type="password" name="password" id="examplePassword" placeholder="enter your password" />
+            </FormGroup>
+            {' '}
+            <Button className="login-button" block color="success">Login</Button>
+          </Form>
+        </div>
+        <div className="RegisterForm">
+        Don't Have Account? <a href="" className="link">Create an Account.</a>
         </div>
       </div>
     )
